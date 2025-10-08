059463a53d2d8f2bb7ac188acd92d08aa7741e4f
paulewinnya25 <paulewinnya@gmail.com>
2025-10-08 15:27:58 +0100

    Config: Ajout page admin au build Vite pour déploiement Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html'
+      }
+    }
   }
 })
 
